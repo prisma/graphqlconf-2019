d9e7cd91fff680a54d07b64d48f0e70ad2e521a5
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-10-19 14:07:19 +0330

    Add Coc

---
+++ code-of-conduct/style.js
@@ -0,0 +1,25 @@
+import styled, { css } from 'styled-components'
+
+// Utils
+import { mobile, desktop } from '../utils/media'
+
+export const Grid = styled.div`
+  display: flex;
+`
+
+export const GridItem = styled.div`
+  ${desktop(css`
+    width: 50%;
+    flex-shrink: 1;
+    margin-right: 100px;
+
+    &:last-child {
+      margin-right: 0;
+    }
+  `)};
+
+  ${mobile(css`
+    width: 100%;
+    margin-bottom: 48px;
+  `)};
+`
