372af4032bcc9c77a41495b4980e93e68acc6d87
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-10-19 13:30:58 +0330

    Add form api

--- shared/Subscribe/index.js
+++ shared/Subscribe/index.js
@@ -14,43 +14,100 @@ import {
   ExapndingText
 } from './style'
 
-export const Subscribe = () => {
-  return (
-    <Container>
-      {/* <SizeMe refreshRate={400} monitorHeight={true}>
-        {({ size }) => (
-          <div
-            style={{
-              position: 'fixed',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%'
-            }}
-          >
-            <Confetti run={true} width={size.width} height={size.height} />
-          </div>
+export class Subscribe extends React.Component {
+  state = { email: '', msg: null, success: null }
+
+  render() {
+    return (
+      <Container>
+        {this.state.success && (
+          <SizeMe refreshRate={400} monitorHeight={true}>
+            {({ size }) => (
+              <div
+                style={{
+                  position: 'fixed',
+                  top: 0,
+                  left: 0,
+                  width: '100vw',
+                  height: '100%'
+                }}
+              >
+                <Confetti run={true} width={size.width} height={size.height} />
+              </div>
+            )}
+          </SizeMe>
         )}
-      </SizeMe> */}
 
-      <Wrapper>
-        <Left>
-          <Desc>
-            <div>
-              <strong>
-                Stay in the <ExapndingText>loop</ExapndingText>
-              </strong>
-            </div>
-            <div>it’s all about GraphQL Conf</div>
-          </Desc>
-        </Left>
+        <Wrapper>
+          <Left>
+            <Desc>
+              <div>
+                <strong>
+                  Stay in the <ExapndingText>loop</ExapndingText>
+                </strong>
+              </div>
+              <div>it’s all about GraphQL Conf</div>
+            </Desc>
+          </Left>
 
-        <Form>
-          <Input placeholder="Add your email" />
-          <Button type="submit">Submit</Button>
-          <Message>We added you!</Message>
-        </Form>
-      </Wrapper>
-    </Container>
-  )
+          <Form onSubmit={this.submitted}>
+            <Input
+              type="email"
+              required={true}
+              placeholder="Add your email"
+              value={this.state.email}
+              onChange={e =>
+                this.setState({ email: e.currentTarget.value, msg: '' })
+              }
+            />
+            <Button type="submit">Submit</Button>
+            {this.state.msg && <Message>{this.state.msg}</Message>}
+          </Form>
+        </Wrapper>
+      </Container>
+    )
+  }
+
+  submitted = e => {
+    e.preventDefault()
+
+    const apiUrl =
+      process.env.NODE_ENV === 'development'
+        ? `http://localhost:3000`
+        : `https://graphqlconf-newsletter.now.sh`
+
+    fetch(apiUrl, {
+      method: 'post',
+      body: JSON.stringify({
+        email: this.state.email
+      })
+    })
+      .then(res => {
+        if (!res.ok) throw res
+        this.setState({
+          msg: `We added you to the list!`,
+          success: true,
+          email: ''
+        })
+        window.setTimeout(() => {
+          this.setState({ success: null })
+        }, 9000)
+      })
+      .catch(async err => {
+        let msg
+
+        const text = (err.text && (await err.text())) || ''
+        if (err.message) {
+          msg = `There was an error: ${err}`
+        } else if (text.includes('already_added')) {
+          msg = `Seems like you're already on the list!`
+        } else {
+          msg = `Please enter a valid email!`
+        }
+
+        this.setState({ msg, success: false })
+      })
+
+    return false
+  }
 }
