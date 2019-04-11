020f0e2c9e4e3fdab6ec8191ae4c267986cc89a0
Nikolas Burk <nikolas.burk@gmail.com>
2019-04-11 10:58:48 +0200

    add prisma day partner

--- home/Partners/index.js
+++ home/Partners/index.js
@@ -21,6 +21,13 @@ export const Partners = () => {
         >
           <ReactImage src="/static/partners/graphql-asia.svg" />
         </LogoWrapper>
+        <LogoWrapper
+          href="https://www.prisma.io/day"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <ReactImage src="/static/partners/prisma-day.svg" />
+        </LogoWrapper>
       </Wrapper>
     </Container>
   )
