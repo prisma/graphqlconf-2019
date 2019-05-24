d242d67407fc82fd187cdc14308d09997cc92b1b
Kuldar <hi@kuldar.com>
2019-05-24 13:19:37 +0200

    Add Hasura

--- home/Sponsors/index.js
+++ home/Sponsors/index.js
@@ -41,6 +41,9 @@ export const Sponsors = () => {
               <LogoImage href="https://www.github.com/">
                 <ReactImage src="/static/sponsors/github.svg" />
               </LogoImage>
+              <LogoImage href="https://hasura.io/">
+                <ReactImage src="/static/sponsors/hasura.svg" />
+              </LogoImage>
               <LogoImage href="https://www.8base.com/">
                 <ReactImage src="/static/sponsors/8base.png" />
               </LogoImage>
