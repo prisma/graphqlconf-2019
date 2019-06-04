3859563ebbd7c3f1d5ed65cdb40b265ee5cabbea
Etel Sverdlov <beigeotter@gmail.com>
2019-06-04 16:52:00 +0200

    added sponsors description

--- sponsors/Companies/index.js
+++ sponsors/Companies/index.js
@@ -11,52 +11,44 @@ export const Companies = () => {
       <Grid>
         <GridItem>
           <Company
-            name="Commerce Tools"
+            name="commercetools"
             logoSrc="/static/sponsors/commercetools.svg"
-            about="Aenean accumsan ultricies neque, ac consequat velit porta eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-            link="https://commercetools.com/" />
-        </GridItem>
-        <GridItem>
-          <Company
-            name="Github"
-            logoSrc="/static/sponsors/github.svg"
-            about="Aenean accumsan ultricies neque, ac consequat velit porta eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-            link="https://github.com/" />
-        </GridItem>
-        <GridItem>
-          <Company
-            name="Shopify"
-            logoSrc="/static/sponsors/shopify.svg"
-            about="Aenean accumsan ultricies neque, ac consequat velit porta eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-            link="https://shopify.com/" />
-        </GridItem>
-        <GridItem>
-          <Company
-            name="Hasura"
-            logoSrc="/static/sponsors/hasura.svg"
-            about="Aenean accumsan ultricies neque, ac consequat velit porta eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-            link="https://hasura.io/" />
+            about="commercetools is a next-generation software company that offers a cloud-based, headless commerce platform, providing the building blocks for the new digital commerce age. commercetools was founded in 2006. Brands such as Bang & Olufsen, Carhartt WIP, Express, and Wizards of the Coast (Hasbro) trust commercetools to power their digital commerce business."
+            link="https://commercetools.com/"
+          />
         </GridItem>
         <GridItem>
           <Company
             name="8base"
             logoSrc="/static/sponsors/8base.png"
-            about="Aenean accumsan ultricies neque, ac consequat velit porta eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-            link="https://8base.com/" />
+            about="8base is built by developers, for developers and trusted by global enterprises. 8base was built to make developers' and innovators' lives easier. The company accomplishes this by continually abstracting away redundant and low-value add work, while never taking away a developer's ability to take full control if they so desire. 
+            "
+            link="https://8base.com/"
+          />
         </GridItem>
         <GridItem>
           <Company
             name="Jahia"
             logoSrc="/static/sponsors/jahia.png"
-            about="Aenean accumsan ultricies neque, ac consequat velit porta eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-            link="https://www.jahia.com/home.html" />
+            about="Jahia makes digital experience simpler. Our open-source applications snap-in to your existing stack for stand-out experiences.  Start with content management and expand to personalize, optimize and scale experiences -- with unified customer data, everywhere."
+            link="https://www.jahia.com/home.html"
+          />
         </GridItem>
         <GridItem>
           <Company
             name="Neo4j"
             logoSrc="/static/sponsors/neo4j.png"
-            about="Aenean accumsan ultricies neque, ac consequat velit porta eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-            link="https://neo4j.com" />
+            about="Neo4j enables organizations to unlock the business value of connections, influences and relationships in data: through new applications that adapt to changing business needs, and by enabling existing applications to scale with the business. Headquartered in San Mateo, USA, Neo4j has offices in Sweden, the UK and Germany."
+            link="https://neo4j.com"
+          />
+        </GridItem>
+        <GridItem>
+          <Company
+            name="Github"
+            logoSrc="/static/sponsors/github.svg"
+            about="GitHub brings together the world's largest community of developers to discover, share, and build better software."
+            link="https://github.com/"
+          />
         </GridItem>
       </Grid>
     </Container>
